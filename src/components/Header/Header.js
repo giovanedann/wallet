8ba085ab20961499237f1dc500a6ece8a30b44af
Giovane <giovanedaniell@gmail.com>
2022-05-16 17:57:16 -0300

    last update before deploy

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import './Header.css';
+import { FaUserCircle } from 'react-icons/fa';
 
 class Header extends Component {
   render() {
     const { email, totalValue } = this.props;
     return (
       <header className="header-container">
-        <p data-testid="email-field" className="email-field">{ email }</p>
+        <div data-testid="email-field" className="email-field">
+          <FaUserCircle color="white" size={ 25 }/>
+          <p>{ email }</p>
+        </div>
         <div className="expenses-value-info">
           <p data-testid="total-field" className="total-field">
             { `Total: $${!totalValue ? 0 : Number(totalValue).toFixed(2)}`}
